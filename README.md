d2f695df872e61e4254f9da8c0f7c3bddb51212a
Dylan Miller <djm2@ualberta.ca>
2025-03-20 11:41:03 -0600

    Refactor tool frame cb for easier usage and include documentation.

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Install Instructions
+From project root
+```bash
+mkdir build && cd build
+cmake ..
+make
+sudo make install
+```
+
+## Integrate with your project
+Add to CMakeLists.txt
+```bash
+find_package(haptip_wrist REQUIRED)
+...
+target_link_libraries(your_target haptic_wrist) 
+```
+See `programs/demo_grav_comp.cpp` and `tool_frame_cb.h` for integrating with the wam.
+## Notes
+
+The joint numbers match the wam wrist, that is J1, J2, J3 correspond with J5, J6, J7 on the wam wrist, respectively.
+
+Make sure wam toolplate dh_params are correct. d should be 0.0
+
+Zero position is set when haptic wrist is powered up, not when process started. Make sure it is close to its zero position (within 1 motor revolution).
+
+If present, reads config files from ~/.config/haptic_wrist . Otherwise reads from /etc/haptic_wrist.
+Overwride the config dir location with env variable HAPTIC_WRIST_CONFIG_DIR
--- programs/demo_grav_comp.cpp
+++ programs/demo_grav_comp.cpp
@@ -4,38 +4,10 @@
 #include <barrett/products/product_manager.h>
 #include <barrett/standard_main_function.h>
 #include <barrett/systems.h>
+#include "tool_frame_cb.h"
 
 BARRETT_UNITS_FIXED_SIZE_TYPEDEFS;
 
-class ToolFrameCb : public barrett::systems::System, public barrett::systems::SingleInput<pose_type> {
-  public:
-    explicit ToolFrameCb(haptic_wrist::HapticWrist *hw, const std::string &sysName = "ToolFrameCb")
-        : System(sysName), SingleInput<pose_type>(this), hw(hw) {}
-    virtual ~ToolFrameCb() { mandatoryCleanUp(); }
-
-  protected:
-    haptic_wrist::HapticWrist *hw;
-
-    virtual void operate() {
-
-        auto wamPose = this->input.getValue();
-        hw->setWristToBase(posQuatToTransform(boost::get<0>(wamPose), boost::get<1>(wamPose)));
-    }
-    Eigen::Matrix4d posQuatToTransform(const Eigen::Vector3d &position, const Eigen::Quaterniond &quaternion) {
-        Eigen::Matrix4d transformation = Eigen::Matrix4d::Identity();
-
-        Eigen::Matrix3d R = quaternion.toRotationMatrix().transpose();
-        transformation.block<3, 3>(0, 0) = R;
-
-        // Set translation
-        transformation.block<3, 1>(0, 3) = -R * position;
-
-        return transformation;
-    }
-
-  private:
-    DISALLOW_COPY_AND_ASSIGN(ToolFrameCb);
-};
 
 template <size_t DOF>
 int wam_main(int argc, char **argv, barrett::ProductManager &pm, barrett::systems::Wam<DOF> &wam) {
@@ -46,10 +18,8 @@ int wam_main(int argc, char **argv, barrett::ProductManager &pm, barrett::system
     hw.gravityCompensate(true);
     hw.run();
 
-    ToolFrameCb toolframeCb(&hw);
-
+    ToolFrameCb toolframeCb(pm.getExecutionManager(), &hw);
     barrett::systems::connect(wam.toolPose.output, toolframeCb.input);
-    pm.getExecutionManager()->startManaging(toolframeCb);
 
     while (true) {
         std::cout << "position\n" << hw.getPosition() << std::endl;
