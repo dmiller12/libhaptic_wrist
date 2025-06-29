a9bdf3fbb80c29659d0fe93119c5de3e646688ba
Dylan Miller <djm2@ualberta.ca>
2025-06-29 09:46:22 -0600

    Add back barrett code

--- programs/demo_grav_comp.cpp
+++ programs/demo_grav_comp.cpp
@@ -1,33 +1,30 @@
-// #include "haptic_wrist/haptic_wrist.h"
-// #include <barrett/units.h>
+#include "haptic_wrist/haptic_wrist.h"
+#include <barrett/units.h>
 
-// #include <barrett/products/product_manager.h>
-// #include <barrett/standard_main_function.h>
-// #include <barrett/systems.h>
-// #include "tool_frame_cb.h"
+#include "tool_frame_cb.h"
+#include <barrett/products/product_manager.h>
+#include <barrett/standard_main_function.h>
+#include <barrett/systems.h>
+BARRETT_UNITS_FIXED_SIZE_TYPEDEFS;
+template <size_t DOF>
+int wam_main(int argc, char** argv, barrett::ProductManager& pm, barrett::systems::Wam<DOF>& wam) {
+    BARRETT_UNITS_TEMPLATE_TYPEDEFS(DOF);
+    wam.gravityCompensate();
 
-// BARRETT_UNITS_FIXED_SIZE_TYPEDEFS;
+    haptic_wrist::HapticWrist hw;
+    hw.gravityCompensate(true);
+    hw.run();
 
+    ToolFrameCb toolframeCb(pm.getExecutionManager(), &hw);
+    barrett::systems::connect(wam.toolPose.output, toolframeCb.input);
 
-// template <size_t DOF>
-// int wam_main(int argc, char **argv, barrett::ProductManager &pm, barrett::systems::Wam<DOF> &wam) {
-//     BARRETT_UNITS_TEMPLATE_TYPEDEFS(DOF);
-//     wam.gravityCompensate();
+    while (true) {
+        std::cout << "position\n" << hw.getPosition() << std::endl;
+        sleep(1);
+    }
 
-//     haptic_wrist::HapticWrist hw;
-//     hw.gravityCompensate(true);
-//     hw.run();
+    pm.getSafetyModule()->waitForMode(barrett::SafetyModule::IDLE);
 
-//     ToolFrameCb toolframeCb(pm.getExecutionManager(), &hw);
-//     barrett::systems::connect(wam.toolPose.output, toolframeCb.input);
-
-//     while (true) {
-//         std::cout << "position\n" << hw.getPosition() << std::endl;
-//         sleep(1);
-//     }
-
-//     pm.getSafetyModule()->waitForMode(barrett::SafetyModule::IDLE);
-
-//     hw.stop();
-//     return 0;
-// }
+    hw.stop();
+    return 0;
+}
