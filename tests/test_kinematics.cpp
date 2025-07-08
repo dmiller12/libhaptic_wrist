8bc01f9e62ec7a225c06fb9ce02663472d06139d
Dylan Miller <djm2@ualberta.ca>
2025-07-08 11:09:11 -0600

    Update test for new exoskel wrist

--- tests/test_kinematics.cpp
+++ tests/test_kinematics.cpp
@@ -9,19 +9,18 @@ class KinematicsTest : public ::testing::Test {
 protected:
     void SetUp() override {
         dh_params = {
-            {-0.5, 0.0, 0.465, 0.0},
-            {0.5,  0.0, 0.0,   0.0},
-            {0.0,  0.0, 0.005, 0.0} 
+            {-0.5, 0.0, 0.37, 0.0},
+            {0.5, 0.0, 0.0, 0.0},
+            {0.0, 0.0, 0.0, 0.0},
         };
 
         world_to_base = Eigen::Matrix4d::Identity();
 
         Eigen::Matrix4d eef_to_tool;
-        eef_to_tool << 0, 0, -1, 0,
-                       0, 1, 0, 0,
-                       1, 0, 0, 0,
+        eef_to_tool << 0, 0,  1, 0,
+                       1.0, 0, 0, 0,
+                       0,  1,  0, 0,
                        0, 0, 0, 1;
-
         // Instantiate the kinematics object for tests
         kinematics = std::make_unique<Kinematics>(dh_params, eef_to_tool, world_to_base);
     }
@@ -38,28 +37,39 @@ TEST_F(KinematicsTest, ForwardKinematicsAtKnownPositions) {
     jp_type pos_at_zero = jp_type::Zero();
     std::array<Kin, 4> kin_zero = kinematics->eval(pos_at_zero);
     Eigen::Matrix4d T_tool_zero = kin_zero[3].to_world_frame;
+    
+    // int i = 0;
+    // for (const Kin& k : kin_zero) {
+    //     std::cout << "frame: " << i << std::endl << k.to_world_frame << std::endl;
+    //     ++i;
+    // }
 
     Eigen::Matrix4d T_expected_zero;
-    T_expected_zero << 0, 0, -1, 0,
-                       0, 1, 0, 0,
-                       1, 0, 0, 0.470,
+    T_expected_zero << 0, 0, 1, 0,
+                       1, 0, 0, 0,
+                       0, 1, 0, 0.370,
                        0, 0, 0, 1;
 
     EXPECT_TRUE(T_tool_zero.isApprox(T_expected_zero, 1e-9));
 
 
     // test j1 90 degrees
-    jp_type pos_j1_90;
-    pos_j1_90 << M_PI / 2.0, 0.0, 0.0;
+    jp_type pos_operator_home;
+    pos_operator_home << -M_PI/2.0, -M_PI/2.0, 0.0;
 
-    std::array<Kin, 4> kin_j1_90 = kinematics->eval(pos_j1_90);
-    Eigen::Matrix4d T_tool_j1_90 = kin_j1_90[3].to_world_frame;
+    std::array<Kin, 4> kin_operator_home = kinematics->eval(pos_operator_home);
+    // i = 0;
+    // for (const Kin& k : kin_operator_home) {
+    //     std::cout << "frame: " << i << std::endl << k.to_world_frame << std::endl;
+    //     ++i;
+    // }
+    Eigen::Matrix4d T_tool_operator_hom = kin_operator_home[3].to_world_frame;
 
-    Eigen::Matrix4d T_expected_j1_90;
-    T_expected_j1_90 <<  0, -1, 0, 0,
-                         0, 0, -1, 0,
-                         1, 0, 0, 0.470,
+    Eigen::Matrix4d T_expected_operator_home;
+    T_expected_operator_home <<  1, 0, 0, 0,
+                         0, 1, 0, 0,
+                         0, 0, 1, 0.370,
                          0, 0, 0, 1;
 
-    EXPECT_TRUE(T_tool_j1_90.isApprox(T_expected_j1_90, 1e-9));
+    EXPECT_TRUE(T_tool_operator_hom.isApprox(T_expected_operator_home, 1e-9));
 }
