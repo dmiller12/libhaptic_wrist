4d2e2f32db6284f6fc67bc2496032acb4a0d559c
Sergey Khlynovskiy <khlynovs@ualberta.ca>
2026-05-15 15:10:44 -0600

    testing more but something is werid

--- programs/simple_commands.cpp
+++ programs/simple_commands.cpp
@@ -10,10 +10,10 @@ int main(int argc, char** argv) {
     hw.run();
 
     sleep(1);
-    hw.hold(true);
-    //
-    // haptic_wrist::jp_type desiredPos;
-    // desiredPos << M_PI / 2.0, M_PI / 2.0, M_PI / 2.0;
+    // hw.hold(true);
+
+    haptic_wrist::jp_type desiredPos;
+    desiredPos << 0, -M_PI / 2.0, 0;
     // Eigen::Quaterniond currentQuat = hw.getOrientation(); 
     //
     // Eigen::Vector3d axis = Eigen::Vector3d::UnitY();
@@ -23,7 +23,7 @@ int main(int argc, char** argv) {
     sleep(1);
     // Eigen::Quaterniond des = hw.getOrientation();
 
-    // hw.setTarget(des);
+    hw.setTarget(desiredPos);
     // hw.setTriggerHaptics(50);
     // hw.setTriggerHaptics(255);
     // std::cout << "set hap" << std::endl;
